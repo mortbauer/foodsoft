1fa917291452c03cc05e8788140650d701f1a308
Martin Ortbauer <mortbauer@gmail.com>
2024-03-09 09:04:35 -0500

    add shield and debug prints for messages create

--- plugins/messages/app/controllers/messages_controller.rb
+++ plugins/messages/app/controllers/messages_controller.rb
@@ -31,13 +31,26 @@ class MessagesController < ApplicationController
 
   # Creates a new message.
   def create
-    @message = @current_user.send_messages.new(params[:message])
-    if @message.save
-      DeliverMessageJob.perform_later(@message)
-      redirect_to messages_url, notice: I18n.t('messages.create.notice')
-    else
-      render action: 'new'
+    puts '_______________________________'
+    ActiveRecord::Base.transaction do
+      @current_user.with_lock do
+        @message = @current_user.send_messages.new(params[:message])
+        puts '------------------------------------------>'
+        puts @message.inspect
+        puts '<..........................................'
+
+        if @message.save
+          DeliverMessageJob.perform_later(@message)
+          redirect_to messages_url, notice: I18n.t('messages.create.notice')
+        else
+          raise ActiveRecord::Rollback # Rollback the transaction if the save fails
+        end
+      end
     end
+    rescue ActiveRecord::RecordInvalid => e
+      # Handle validation errors
+      render action: 'new'
+    puts '_______________________________<<<<<<'
   end
 
   # Shows a single message.
